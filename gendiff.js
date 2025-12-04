832b4e4f7ac137c7f2fcddc65092eb3a96536460
Alexandr Ananev <sanyches1978@gmail.com>
2025-12-04 13:03:01 +0100

    feat(gendiff.js): add parsing of two JSON files

--- gendiff.js
+++ gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander'
+import { parseFile } from './parser.js'
 
 const program = new Command()
 
@@ -8,7 +9,22 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format')
-  .helpOption('-h, --help', 'display help for command')
-  .parse()
+
+program
+  .argument('<filepath1>', 'path to first file')
+  .argument('<filepath2>', 'path to second file')
+  .action((filepath1, filepath2) => {
+    try {
+      const data1 = parseFile(filepath1)
+      const data2 = parseFile(filepath2)
+
+      console.log('File1 data:', data1)
+      console.log('File2 data:', data2)
+    }
+    catch (error) {
+      console.error(error.message)
+      process.exit(1)
+    }
+  })
+
+program.parse(process.argv)
