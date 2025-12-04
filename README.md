b5836f0b1a71487e979e1e8cc19b81a97923efd9
Alexandr Ananev <sanyches1978@gmail.com>
2025-12-04 15:35:42 +0100

    fix README.md, gendiff.js

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Alexandr-Ananev/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Alexandr-Ananev/frontend-project-46/actions)
+
+[![asciicast](https://asciinema.org/a/gQSAHEvTZbXryt8udVEvUIqOB.svg)](https://asciinema.org/a/gQSAHEvTZbXryt8udVEvUIqOB)
--- gendiff.js
+++ gendiff.js
@@ -2,8 +2,30 @@
 
 import { Command } from 'commander'
 import { parseFile } from './parser.js'
+import _ from 'lodash'
 
 const program = new Command()
+
+const getSortedKeys = (obj1, obj2) => _.sortBy([...new Set([...Object.keys(obj1), ...Object.keys(obj2)])])
+
+const genDiff = (data1, data2) => {
+  const keys = getSortedKeys(data1, data2)
+
+  const lines = keys.flatMap((key) => {
+    if (!Object.prototype.hasOwnProperty.call(data2, key)) {
+      return [`- ${key}: ${data1[key]}`]
+    }
+    if (!Object.prototype.hasOwnProperty.call(data1, key)) {
+      return [`+ ${key}: ${data2[key]}`]
+    }
+    if (_.isEqual(data1[key], data2[key])) {
+      return [`  ${key}: ${data1[key]}`]
+    }
+    return [`- ${key}: ${data1[key]}`, `+ ${key}: ${data2[key]}`]
+  })
+
+  return `{\n${lines.join('\n')}\n}`
+}
 
 program
   .name('gendiff')
@@ -18,8 +40,8 @@ program
       const data1 = parseFile(filepath1)
       const data2 = parseFile(filepath2)
 
-      console.log('File1 data:', data1)
-      console.log('File2 data:', data2)
+      const diff = genDiff(data1, data2)
+      console.log(diff)
     }
     catch (error) {
       console.error(error.message)
